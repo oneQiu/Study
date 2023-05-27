4e1dd7431dfae149164a9182d4d9a83b8f12bb1c
QiuQiu <50658231+oneQiu@users.noreply.github.com>
2023-05-27 09:45:38 +0000

    Update 模块化开发

--- 设计模式/index.js
+++ 设计模式/index.js
@@ -1,18 +1,18 @@
 // 单例模型
 // ES5
 const CreateSingleton = (() => {
-    let instance;
-    return function (name) {
-        if (instance) return instance;
-        this.name = name;
-        return instance = this;
-    }
-})()
+	let instance;
+	return function (name) {
+		if (instance) return instance;
+		this.name = name;
+		return (instance = this);
+	};
+})();
 
 CreateSingleton.prototype.getName = function () {
-    return this.name;
-}
+	return this.name;
+};
 
 const sing = new CreateSingleton('QiuQiu');
 const sing2 = new CreateSingleton('WiuWiu');
-console.log(sing.getName(), sing2.getName())
+console.log(sing === sing2);
