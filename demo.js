4e5b40f0120a3912d3c2308eacd8cbeca0031ccc
QiuQiu <50658231+oneQiu@users.noreply.github.com>
2023-05-29 08:57:07 +0000

    Update 缓存+存储

--- demo.js
+++
@@ -1,9 +0,0 @@
-const someAsyncThing = function () {
-  return new Promise(function (resolve, reject) {
-    // 下面一行会报错，因为x没有声明
-    resolve(x + 2);
-  });
-};
-
-someAsyncThing()
-console.log(12312)
