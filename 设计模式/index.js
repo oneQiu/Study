3dd5c3f44b1ee65f2d19e8287f3a02831ff90191
QiuQiu <qq982516861@gmail.com>
2023-07-05 16:41:38 +0800

    Update

--- 设计模式/index.js
+++ 设计模式/index.js
@@ -1,18 +1,7 @@
-// 单例模型
-// ES5
-const CreateSingleton = (() => {
-	let instance;
-	return function (name) {
-		if (instance) return instance;
-		this.name = name;
-		return (instance = this);
-	};
-})();
+class Test {
+	static a = 123;
+}
 
-CreateSingleton.prototype.getName = function () {
-	return this.name;
-};
-
-const sing = new CreateSingleton('QiuQiu');
-const sing2 = new CreateSingleton('WiuWiu');
-console.log(sing === sing2);
+new Test();
+Test.a = '1234';
+console.log(Test.a);
